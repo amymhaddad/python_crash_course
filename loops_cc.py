b378e39875e4604f50d281730df533f23f159320
Amy Haddad <amy.m.haddad@gmail.com>
2018-08-06 11:15:44 -0400

    Update loops_cc.py

--- loops_cc.py
+++ loops_cc.py
@@ -12,26 +12,16 @@ for value in range(1,20):
 
 
 #Make a list of numbers from 1 to one million and then use a for loop to print the numbers
-#option1:
 numbers = list(range(1, 1000000))
 print(numbers)
 
 numbers = []
-for value in range(1, 10):
-    numbers.append(value)
-print(numbers)
-
-#alternative:
-numbers = []
-for value in range(1,1000000):
+for value in range(1, 1000000):
     numbers.append(value)
 print(numbers)
 
 
 #Make a list of numbers from one to one million, then use min(), max(), and sum()
-numbers = list(range(1,1000000))
-print(numbers)
-
 numbers = [1, 1000000]
 print(min(numbers))
 print(max(numbers))
@@ -39,11 +29,16 @@ print(sum(numbers))
 
 
 #Use the third argument of the range() function to make a list of the odd numbers from 1 to 20. Use a for loop to print each number.
-numbers = list(range(1,20,2))
-print(numbers)
 
 for value in range(1,20,2):
     print(value)
+
+new_list = list(range(1,20,2))
+print(new_list) 
+
+#alternative:
+numbers = list(range(1,20,2))
+print(numbers)
 
 #alternative:
 numbers = []
@@ -53,17 +48,17 @@ print(numbers)
 
 
 #Make a list of the multiples of 3 from 3 to 30. Use a for loop to print the numbers in your list.
-numbers = list(range(3,30,3))
-print(numbers)
-
 for value in range(3,30,3):
     print(value)
 
+new_list = list(range(3,30,3))
+print(new_list)
+
 #alternative:
-numbers = []
+second_list = []
 for value in range(3,30,3):
-    numbers.append(value)
-print(numbers)
+    second_list.append(value)
+print(second_list)
 
 
 #Make a list of the first 10 cubes (ie, the cube of each integer from 1-10), and use a for loop to print the value of each cube
