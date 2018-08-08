abed286a419386451719929440d5117ffdf27c80
Amy Haddad <amy.m.haddad@gmail.com>
2018-08-08 10:49:47 -0400

    ccpage33.py
    There are several short exercises listed in this file. The instructions for each exercise are listed as comments. I intentionally use a double space to separate the exercises.

---
+++ ccpage33.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 10 15:34:22 2018
+
+@author: amyhaddad
+"""
+
+#2-8. Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8 .
+print(f"4 + 4 = {4 + 4}")
+print(f"10 - 2 = {10 - 2}")
+print(f"4 * 4 = {4 * 4}")
+print(f"16 / 4 = {int(16 / 4)}")
+
+
+#2-9. Favorite Number: Store your favorite number in a variable . 
+# Then, using that variable, create a message that reveals your favorite number . Print that message .
+fav_number = 11
+print(str(fav_number) + " is my favorite number.")
