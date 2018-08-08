9406b3ebe9ea02ce5326e2a25653b9f5b7e3b892
Amy Haddad <amy.m.haddad@gmail.com>
2018-08-08 10:48:51 -0400

    Update ccpage29.py

--- ccpage29.py
+++ ccpage29.py
@@ -11,7 +11,7 @@ name = "amy"
 print(f"Hello, {name.capitalize()}. Are ready for the test?")
 
 
-#2-4. Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase .
+#2-4. Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
 name = "amy haddad"
 
 print(name.lower())
@@ -19,27 +19,27 @@ print(name.upper())
 print(name.title())
 
 
-#2-5. Famous Quote: Find a quote from a famous person you admire . Print the quote and the name of its author . 
+#2-5. Famous Quote: Find a quote from a famous person you admire . Print the quote and the name of its author. 
 famous_quote = "Continuous effort -- not strength or intellgence -- is the key to unlocking our potential."
 quote_author = "winston churchill"
 
 print(f"{quote_author.title()} said,\"{famous_quote}\"")
 
 
-#2-6. Repeat Exercise 2-5, but this time store the famous per- son’s name in a variable called famous_person . 
-# Then compose your message and store it in a new variable called message . Print your message .
+#2-6. Repeat Exercise 2-5, but this time store the famous per- son’s name in a variable called famous_person. 
+# Then compose your message and store it in a new variable called message. Print your message.
 famous_person = "winston churchill"
 message = (f"{famous_person.capitalize()} said, \"{famous_quote}\"")
 print(message)
 
 
-#2-7. Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name . 
+#2-7. Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name. 
 # Make sure you use each character combination, "\t" and "\n", at least once
-#Print the name once, so the whitespace around the name is displayed .
+#Print the name once, so the whitespace around the name is displayed.
 names = "\tamy haddad\t"
 print(names)
 
-# Then print the name using each of the three stripping functions, lstrip(), rstrip(), and strip() .
+# Then print the name using each of the three stripping functions, lstrip(), rstrip(), and strip().
 names = names.rstrip()
 print(names)
 
