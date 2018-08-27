956f5422c1874fdd867da83024acdc9602b5e5cb
Amy Haddad <amy.m.haddad@gmail.com>
2018-08-27 11:03:51 -0400

    Update ccpage40.py

--- ccpage40.py
+++ ccpage40.py
@@ -16,7 +16,7 @@ print(names[-1].capitalize())
 #3-2. Greetings: Start with the list you used in Exercise 3-1, but instead of just printing each person’s name, print a message to them.
 print(f"Welcome to Texas, {names[0].capitalize()}!")
 print(f"{names[1].capitalize()}, here's the list of ingredients:\nmilk,\nbread, and\ncheese.")
-print(f"{names[-1].capitalize()}, let's go to the game today.")
+print(f"{names[2].capitalize()}, let's go to the game today.")
 
 
 #3-3. Your Own List: Think of your favorite mode of transportation
@@ -26,6 +26,6 @@ transportation = ['walk', 'train', 'car', 'uber']
 fav_transport = transportation[0]+"ing"
 
 print(f"{fav_transport.capitalize()} is my preferred mode of transportation.")
-print(f"I often take an {transportation[-1].capitalize()} ride to far away places.")
+print(f"I often take an {transportation[3].capitalize()} ride to far away places.")
 print(f"The {transportation[1]} is a fast way to travel.")
 print(f"I don't like to travel by {transportation[2]}.")
