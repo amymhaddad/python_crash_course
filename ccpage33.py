5011818a6bcb9ddc2a10d436a9b1ba5297759530
Amy Haddad <amy.m.haddad@gmail.com>
2018-08-27 11:12:25 -0400

    Update ccpage33.py

--- ccpage33.py
+++ ccpage33.py
@@ -9,8 +9,8 @@ Created on Thu May 10 15:34:22 2018
 #2-8. Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8 .
 print(f"4 + 4 = {4 + 4}")
 print(f"10 - 2 = {10 - 2}")
-print(f"4 * 4 = {4 * 4}")
-print(f"16 / 4 = {int(16 / 4)}")
+print(f"4 * 2 = {4 * 2}")
+print(f"16 / 2 = {int(16 / 2)}")
 
 
 #2-9. Favorite Number: Store your favorite number in a variable . 
