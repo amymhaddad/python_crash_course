5187431a37e6db6b0e4b4c9be40947842617f62b
Amy Haddad <amy.m.haddad@gmail.com>
2018-09-21 07:54:56 -0400

    Update user_class.py

--- user_class.py
+++ user_class.py
@@ -9,6 +9,8 @@ Created on Thu May 10 15:34:22 2018
 """A class that can be used to represent a user"""
 
 class User():
+
+
     """"An attempt to model a user"""
     def __init__(self, first_name, last_name):
         """Initialize the attributes"""
@@ -19,3 +21,4 @@ class User():
         """Summarize the user's first and last names"""
         full_name = self.first_name.title() + ' ' + self.last_name.title()
         return full_name
+        
