7da1d68053009cb979ffe47cbdf126b4f6312849
Amy Haddad <amy.m.haddad@gmail.com>
2018-09-26 15:20:08 -0400

    Update cats_and_dogs.py

--- cats_and_dogs.py
+++ cats_and_dogs.py
@@ -12,7 +12,7 @@ Created on Thu May 10 15:34:22 2018
 # Wrap your code in a try-except block to catch the FileNotFound error, and print a friendly message if a file is missing. 
 # Move one of the files to a dif- ferent location on your system, and make sure the code in the except block executes properly.
 
-1 way to solve this exercise: 
+# One way to solve this exercise:
 filenames = ['cats.txt', 'dogs.txt']
 for filename in filenames:
     try:
@@ -21,18 +21,19 @@ for filename in filenames:
             print(f"The names from {filename} are:")
             print(contents.title())
             print("\n")
-    except FileNotFoundError: 
+    except FileNotFoundError:
         print(f"The file {filename} is not found.")
 
-# Second way to solve this exercise: 
+# Second way to solve this exercise:
 def animal_names(filename):
+
     """Print the names of the cats and dogs listed in separate files"""
     try: 
         with open(filename) as f_object:
             contents = f_object.read()
-    except FileNotFoundError: 
+    except FileNotFoundError:
         print(f"The file {filename} is not found.")
-    else: 
+    else:
         print(f"The names in {filename} are: \n{contents.title()}\n")
 
 filenames = ['cats.txt', 'dogs.txt']
@@ -41,15 +42,18 @@ for filename in filenames:
 
 # Modify your except block in Exercise 10-8 to fail silently if either file is missing.
 def animal_names(filename):
+
     """Print the names of the cats and dogs listed in separate files"""
-    try: 
+    try:
         with open(filename) as f_object:
             contents = f_object.read()
     except FileNotFoundError: 
         pass
-    else: 
+    else:
         print(f"The names in {filename} are: \n{contents.title()}\n")
 
 filenames = ['cats.txt', 'dogs.txt']
+
 for filename in filenames:
     animal_names(filename)
+
