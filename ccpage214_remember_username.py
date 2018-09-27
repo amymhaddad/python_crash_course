100531790c55cb74e26c1625485ddd631e9f61ad
Amy Haddad <amy.m.haddad@gmail.com>
2018-09-27 12:42:45 -0400

    ccpage214_remember_username.py
    Instructions: 
    The final listing for remember_me.py assumes either that the user has already entered their username or that the program is running for the first time . We should modify it in case the current user is not the person who last used the program .
    Before printing a welcome back message in greet_user(), ask the user if this is the correct username . If it’s not, call get_new_username() to get the correct username .

---
+++ ccpage214_remember_username.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 10 15:34:22 2018
+
+@author: amyhaddad
+"""
+
+import json
+
+def get_username():
+    """Get the username, if one exists"""
+
+    filename = 'username1.json'
+    try: 
+        with open(filename) as f_obj:
+            username = json.load(f_obj)
+    except FileNotFoundError:
+        None
+    else: 
+        return username
+
+def create_username():
+    """Create a username, if one doesn't exist"""
+
+    filename = 'username1.json'
+    username = input("Enter your username: ")
+
+    with open(filename, 'w') as f_obj:
+        json.dump(username, f_obj)
+    return username
+
+def greet_user():
+    """Greet the user and make sure the users are the same people"""
+
+    username = get_username()
+
+    verify_username = input(f"Is your username {username}? Type \'yes\' or \'no\'. ")
+
+    if verify_username == 'yes':
+        print(f"Welcome back, {username}!")
+    else: 
+        username = create_username()
+        print(f"We'll remember you next time, {username}.")
+
+greet_user()
