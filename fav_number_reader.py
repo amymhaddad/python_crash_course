c576fcbad4ae7403e2799d0bf1cb8ab54a1e1576
Amy Haddad <amy.m.haddad@gmail.com>
2018-09-27 12:44:23 -0400

    Update fav_number_reader.py

--- fav_number_reader.py
+++ fav_number_reader.py
@@ -6,6 +6,9 @@ Created on Thu May 10 15:34:22 2018
 @author: amyhaddad
 """
 
+# Write a program that prompts for the user’s favorite number.
+# Use json.dump() to store this number in a file.
+# Write a separate pro- gram that reads in this value and prints the message, “I know your favorite number! It’s _____ .”
 import json
 
 filename = 'fav_number.json'
